cdf8a23ad0636ae28f3432387c5295109fca0443
Marcus Bullock <37095667+Mrtbull@users.noreply.github.com>
2020-11-09 22:44:40 -0500

    Add files via upload

---
+++ chapter1assignment8/chapter1assignment8/src/chapter1assignment8/Assignment8.java
@@ -0,0 +1,14 @@
+package chapter1assignment8;
+
+public class Assignment8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("     J        A       V       V       A");
+System.out.println("     J       A A       V     V       A A");
+System.out.println("     J      A   A       V   V       A   A");
+System.out.println("J    J     AAAAAAA       V V       AAAAAAA");
+System.out.println("JJJJJJ    A       A       V       A       A");
+	}
+
+}
