b68622ee7eeeb01d766d2b66487e6778b96beec7
Marcus Bullock <37095667+Mrtbull@users.noreply.github.com>
2020-11-09 23:27:47 -0500

    Add files via upload

---
+++ Vegetables/src/Corn.java
@@ -0,0 +1,18 @@
+Import Java.util.Scanner;
+public class Corn extends SuperCorn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	Vegetable.Supercorn sc = new Vegetable.SuperCorn();
+	Corn c = new Corn();
+	System.out.println(c.getName());
+	System.out.println(c.getNumber());
+	Vegetable.SuperCorn ssc = new Vegetable.SupcerCorn("Purple");
+	System.out.println();
+	}
+	public int getNmber()
+	{
+		return 2;
+	}
+	
+}
---
+++ Vegetables/src/SuperCorn.java
@@ -0,0 +1,30 @@
+public class SuperCorn {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+private String brand;
+
+public SuperCorn()
+{
+brand = "delmonte";
+}
+public SuperCorn(String myBrand)
+{
+	brand = myBrand;
+}
+
+	public int getVegetableNum()
+	{
+		return 2;
+	
+	}
+	public String toString()
+	{
+		return brand;
+		
+	}
+	
+}
+
