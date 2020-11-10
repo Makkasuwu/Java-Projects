5bd5a3388dab0224400e3a87edbff08f4333c717
Marcus Bullock <37095667+Mrtbull@users.noreply.github.com>
2020-11-09 23:26:25 -0500

    Add files via upload

---
+++ Bank/src/BankAccount.java
@@ -0,0 +1,42 @@
+
+public class BankAccount {
+private String Name;
+private int AccNum;
+private double AccBalance;
+
+public BankAccount()
+{
+	Name = "tyler";
+	AccNum = 125425;
+	AccBalance = 0;
+	
+}
+	public void setName(String henry)
+	{
+		Name = henry;
+	}
+	public void setNum(int acnu)
+	{
+		AccNum = acnu;
+	}
+	public void setBalance(double bal)
+	{
+		AccBalance = bal;
+	}
+	public double getBalance()
+	{
+		return AccBalance;
+	}
+	public double deductMonthlyBalance()
+	{
+		return AccBalance;
+	}
+	
+	//String explainAccountPolicy=("$4 service charge will be deducted every month.");
+	
+	
+	
+	}
+	
+
+
---
+++ Bank/src/TestBankAccount.java
@@ -0,0 +1,31 @@
+
+public class TestBankAccount {
+
+	public static void main(String[] args) {
+	String user1;
+	String user2;
+	String user3;
+	
+	
+	public static void getData(acc1);
+	{
+		
+	}
+	public static void getData(user2);
+	{
+		
+	}
+	public static void getData(user3);
+	{
+		
+	}
+	
+	}
+
+	showValues()
+	{
+		
+	}
+		
+	}
+
