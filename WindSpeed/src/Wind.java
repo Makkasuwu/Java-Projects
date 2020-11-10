fff7efd448f4a79199490aeda7b25297ce678a47
Marcus Bullock <37095667+Mrtbull@users.noreply.github.com>
2020-11-09 23:28:00 -0500

    Add files via upload

---
+++ WindSpeed/src/Wind.java
@@ -0,0 +1,20 @@
+
+public class Wind {
+ 
+	
+ private int sum;
+ private int highest;
+ private int lowest;
+ private int windspeed;
+
+ 
+{
+ 
+ for(windspeed=1; windspeed+; update)
+ {
+    //Statements
+ }
+ 
+
+}
+}
---
+++ WindSpeed/src/WindTest.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+public class WindTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int WindSpeed;
+		int sum;
+		
+		Wind s = new Wind();
+		
+		Scanner keyboard = new Scanner(System.in);
+
+		System.out.println("What is the wind speed?");
+		 WindSpeed = keyboard.nextInt();
+		
+			}
+
+}
