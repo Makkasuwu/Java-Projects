c3a16f68950db02cfa387011a3d17c1d7d7901de
Marcus Bullock <37095667+Mrtbull@users.noreply.github.com>
2020-11-09 22:38:50 -0500

    Add files via upload

---
+++ Chapter1assignment9/Chapter1assignment9/src/bohemianRhapsody/FavoriteSong.java
@@ -0,0 +1,13 @@
+package bohemianRhapsody;
+
+public class FavoriteSong {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Is this the real life?");
+		System.out.println("Or is this just fantasy?");
+		System.out.println("Caught in a land slide");
+		System.out.println("No escape from reality");
+	}
+
+}
